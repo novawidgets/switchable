4f19c479ed88fb673194c3b31ce5a53da1ca8cb2
melonHuang <melonh0327@gmail.com>
2014-04-03 12:07:38 +0800

    update to 1.0.2, update build tool

--- build/tab.1.0.1.js
+++ build/tab.1.0.1.js
@@ -1,3 +1,86 @@
+/* Version 1.0.1 */
+
+(function() {
+    var Switchable = Widget.extend({
+        /* 默认配置 */
+        attrs: {
+            index: -1,          // 初始选项
+            count: 0,           // 选项个数
+            recursive: false    // 是否可循环
+        },
+
+        /* 初始化 */
+        setup: function() {
+        },
+
+        /* 切换到下一个，成功返回true, 触发next事件 */
+        next: function() {
+            var index = this.get('index');
+            var next= this.getNextIndex();
+            if(next != -1) {
+                this.switch(next);
+                return true;
+            }
+            return false;
+        },
+
+        /* 切换到上一个，成功返回true, 触发prev事件 */
+        prev: function() {
+            var index = this.get('index');
+            var prev = this.getPrevIndex();
+            if( prev != -1) {
+                this.switch(prev);
+                return true;
+            }
+            return false;
+        },
+
+        getNextIndex: function() {
+            var from = this.get('index'),
+                to = -1, 
+                count = this.get('count');
+            if(this.get('recursive')) {
+                to = (from + 1) % count;
+            }
+            // 不可循环，但未超出范围
+            else if((from + 1) < count){
+                to = from + 1;
+            }
+
+            return to;
+        },
+
+        getPrevIndex: function() {
+            var from = this.get('index'),
+                to = -1, 
+                count = this.get('count');
+            if(this.get('recursive')) {
+                to = (from + count - 1) % count;
+            }
+            // 不可循环，但未超出范围
+            else if((from - 1) >= 0){
+                to = from - 1;
+            }
+
+            return to;
+
+        },
+
+        /* 切换到to, 成功返回true, 触发switch事件 */
+        switch: function(to) {
+            var from = this.get('index');
+            if(from != to) {
+                this.set('index', to)
+                //this.trigger('switch', [from, to]);
+                return true;
+            }
+            return false;
+        },
+    });    
+
+    this.Switchable = Switchable;
+})(); 
+
 (function() {
     var prefix = (function () {
         var styles = window.getComputedStyle(document.documentElement, ''),
@@ -311,4 +394,33 @@
     /***************************** End of Class Carousel ******************************/
 
     this.Carousel = Carousel;
+})();
+
+(function() {
+
+    Tab = Carousel.extend({
+        attrs: {
+            animate: true,
+            recursive: false,
+            autoplay: false,
+
+            selectors: {
+                content: '.tab-cont',
+                contItem: '.cont-item',
+                control: '.tab-control',
+                controlItem: '.control-item',
+                active: '.active'
+            },
+        },
+        setup: function() {
+            if(!this.get('animate')) {
+                this.set('swipable', false);
+                this.set('duration_ms', 0);
+            }
+            Tab.superclass.setup.call(this);
+        }
+    });
+
+    this.Tab = Tab;
+
 })();
